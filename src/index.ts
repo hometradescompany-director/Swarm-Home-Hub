5cc12baa8b535c055e97b5f123f2459a86007638
Jarrod R T Cobb <hometradescompany@gmail.com>
2026-09-20 12:22:07 +1000

    web: export hardened transport

--- src/index.ts
+++ src/index.ts
@@ -31,3 +31,5 @@ export { ResidenceRequestService as RequestService } from "./service/request-ser
 export * from "./platform/swarm-home-door.js";
 export * from "./platform/tool-manifest.js";
 export * from "./platform/tool-router.js";
+
+export * from "./platform/web-transport.js";
