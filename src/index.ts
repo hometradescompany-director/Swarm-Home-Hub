36422b265bc43cc0666a51ae3c225f56d49afab1
Jarrod R T Cobb <hometradescompany@gmail.com>
2026-09-20 12:07:21 +1000

    086/666 Export the Swarm Home web transport

--- src/index.ts
+++ src/index.ts
@@ -31,3 +31,4 @@ export { ResidenceRequestService as RequestService } from "./service/request-ser
 export * from "./platform/swarm-home-door.js";
 export * from "./platform/tool-manifest.js";
 export * from "./platform/tool-router.js";
+export * from "./platform/web-transport.js";
