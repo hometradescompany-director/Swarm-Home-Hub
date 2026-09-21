afdd94fdeed4179a9c77044ae6dddb956c075ed5
Jarrod R T Cobb <hometradescompany@gmail.com>
2026-09-21 13:50:35 +1000

    feat: export operator agency preservation surface

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,8 @@ export * from "./integrations/atlas/federation.js";
 export * from "./integrations/atlas/http-gateway.js";
 export * from "./integrations/atlas/event-sink.js";
 export * from "./integrations/atlas/event-delivery.js";
+export * from "./integrations/atlas/operator-agency.js";
+export * from "./policy/operator-preservation.js";
 export { ResidenceRequestService as RequestService } from "./service/request-service.js";
 
 export * from "./platform/swarm-home-door.js";
